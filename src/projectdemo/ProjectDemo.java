0387fb7ace8ecf92243d6834ee9332a98809329a
rashadvahid <rashad.vahidli@gmail.com>
2023-08-31 15:40:49 +0400

    deyishiklik

--- src/projectdemo/ProjectDemo.java
+++ src/projectdemo/ProjectDemo.java
@@ -2,6 +2,9 @@ package projectdemo;
 
 public class ProjectDemo {
     public static void main(String[] args) {
-        
+        System.out.println("Salam");
+    }
+    public static void foo(){
+        System.out.println("aa");
     }
 }
